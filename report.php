294e951e790cb554301631b5116bdc2a0dd2d680
Jolo Tadeo <lucc.valo@gmail.com>
2024-10-21 18:58:29 +0800

    asdasd

--- report.php
+++ report.php
@@ -25,7 +25,6 @@
                             <label>Report</label>
                             <div class="form-group" id="manualPurokInput" style="display: none;">
                                 <input type="text" id="otherPurok" name="otherPurok" placeholder="Enter purok">
-                                <p>tae</p>
                             </div>
                         </div>
 
