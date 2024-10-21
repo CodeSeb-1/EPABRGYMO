542ff23cfad535a8550a8cf50f43fdbc62fafdd5
Jolo Tadeo <lucc.valo@gmail.com>
2024-10-21 17:49:05 +0800

    updated

--- userController/report_controller.php
+++ userController/report_controller.php
@@ -1,12 +1,29 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'].'/EPABRGYMO/includes/model.php');
+session_start();
+include_once($_SERVER['DOCUMENT_ROOT'] . '/EPABRGYMO/includes/model.php');
 
-if(isset($_POST['report'])) {
-    $report = $_POST['report'];
-    $description = $_POST['description'];
-    $person = $_POST['person'];
+if (isset($_POST['report'])) {
+    $report_type = $_POST['report_type'];
+    $report_content = $_POST['description'];
+    $report_name = $_POST['person'];
+    $report_purok = ($_POST['purokOption'] === "sameAsMe") ? $_SESSION['user_purok'] : $_POST['otherPurok'];
+    $user_id = $_SESSION['user_id'];
 
-    $insert = [
-        'query' => 'INSERT INTO reports'
+    // Insert query configuration
+    $insert_report = [
+        'query' => "INSERT INTO reports (user_id, report_type, report_content, report_name, report_purok) 
+                    VALUES (?, ?, ?, ?, ?)",
+        'bind' => 'isssi',
+        'value' => [$user_id, $report_type, $report_content, $report_name, $report_purok]
     ];
+
+    // Execute the insertion
+    $result = insertData($insert_report);
+
+    // Handle success or failure
+    if ($result) {
+        echo "<script>alert('Report submitted successfully!'); window.location.href='../report.php';</script>";
+    } else {
+        echo "<script>alert('Error: Report could not be processed.'); window.location.href='../report.php';</script>";
+    }
 }
